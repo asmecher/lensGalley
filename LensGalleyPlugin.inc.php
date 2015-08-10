125a0270c3fdf364cad75a67c2c0ed426a65408c
Alec Smecher <alec@smecher.bc.ca>
2015-08-10 16:12:06 -0700

    Rewrite lens plugin display to support issue galleys too

--- LensGalleyPlugin.inc.php
+++ LensGalleyPlugin.inc.php
@@ -32,15 +32,44 @@ class LensGalleyPlugin extends ViewableFilePlugin {
 	}
 
 	/**
+	 * Determine whether this plugin can handle the specified content.
+	 * @param $galley ArticleGalley|IssueGalley
+	 * @return boolean True iff the plugin can handle the content
+	 */
+	function canHandle($galley) {
+		if (is_a($galley, 'ArticleGalley') && $galley->getGalleyType() == $this->getName()) {
+			return true;
+		} elseif (is_a($galley, 'IssueGalley') && $galley->getFileType() == 'application/xml') {
+			return true;
+		}
+		return false;
+	}
+
+	/**
 	 * @see ViewableFilePlugin::displayArticleGalley
 	 */
-	function displayArticleGalley($templateMgr, $request, $params) {
-		$templateMgr->assign('pluginLensPath', $this->getLensPath($request));
-		$galley = $templateMgr->get_template_vars('galley');
+	function displayArticleGalley($request, $issue, $article, $galley) {
+		$templateMgr = TemplateManager::getManager($request);
 		$galleyFiles = $galley->getLatestGalleyFiles();
 		assert(count($galleyFiles)==1);
-		$templateMgr->assign('firstGalleyFile', array_shift($galleyFiles));
-		return parent::displayArticleGalley($templateMgr, $request, $params);
+		$templateMgr->assign(array(
+			'pluginLensPath' => $this->getLensPath($request),
+			'firstGalleyFile' => array_shift($galleyFiles),
+			'pluginTemplatePath' => $this->getTemplatePath(),
+		));
+		return parent::displayArticleGalley($request, $issue, $article, $galley);
+	}
+
+	/**
+	 * @see ViewableFilePlugin::displayArticleGalley
+	 */
+	function displayIssueGalley($request, $issue, $galley) {
+		$templateMgr = TemplateManager::getManager($request);
+		$templateMgr->assign(array(
+			'pluginLensPath' => $this->getLensPath($request),
+			'pluginTemplatePath' => $this->getTemplatePath(),
+		));
+		return parent::displayIssueGalley($request, $issue, $galley);
 	}
 
 	/**
@@ -50,6 +79,14 @@ class LensGalleyPlugin extends ViewableFilePlugin {
 	 */
 	function getLensPath($request) {
 		return $request->getBaseUrl() . '/' . $this->getPluginPath() . '/lib/lens';
+	}
+
+	/**
+	 * Get the template path
+	 * @return string
+	 */
+	function getTemplatePath() {
+		return parent::getTemplatePath() . 'templates/';
 	}
 }
 
---
+++ templates/display.tpl
@@ -0,0 +1,26 @@
+{**
+ * plugins/viewableFile/lensGalley/display.tpl
+ *
+ * Copyright (c) 2014-2015 Simon Fraser University Library
+ * Copyright (c) 2003-2015 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * Embedded viewing of a PDF galley.
+ *}
+<script src="{$pluginLensPath}/lens.js"></script>
+<script type="text/javascript">{literal}
+
+	var linkElement = document.createElement("link");
+	linkElement.rel = "stylesheet";
+	linkElement.href = "{/literal}{$pluginLensPath|escape:"javascript"}{literal}/lens.css"; //Replace here
+
+	document.head.appendChild(linkElement);
+
+	$(document).ready(function(){
+		var app = new Lens({
+			document_url: "{/literal}{$xmlUrl|escape:'javascript'}{literal}"
+		});
+		app.start();
+		window.app = app;
+	});
+{/literal}</script>
