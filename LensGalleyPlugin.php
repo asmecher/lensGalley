8e25d43d1f91e04edbd64a4875a0950ea2c43725
Jonas Raoni Soares da Silva <github@raoni.org>
2023-03-21 13:10:05 +0300

    Updated header

--- LensGalleyPlugin.php
+++ LensGalleyPlugin.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @file plugins/generic/lensGalley/LensGalleyPlugin.inc.php
+ * @file plugins/generic/lensGalley/LensGalleyPlugin.php
  *
  * Copyright (c) 2014-2021 Simon Fraser University
  * Copyright (c) 2003-2021 John Willinsky
